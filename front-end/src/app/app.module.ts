eb3ad320cb0c136561f4c16a943c476d498b2a8c
Pank1910 <nlphuonganh.work@gmail.com>
2025-02-04 22:32:35 +0700

    4/2 add header

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 
 // Import các trang mới
 import { HomepageComponent } from './pages/homepage/homepage.component';
