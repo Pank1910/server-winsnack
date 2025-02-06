bf5a54fea34ca7bb71f06f2b7d61c4552a90fbbe
DuyenAn <antvd22411c@st.uel.edu.vn>
2025-02-06 23:56:31 +0700

    Update 2 trang product details và account ngày 06/02

--- front-end/src/app/pages/product-detail/product-detail.component.spec.ts
+++ front-end/src/app/pages/product-detail/product-detail.component.spec.ts
@@ -1,23 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
-import { ProductDetailComponent } from './product-detail.component';
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent {
+  product = {
+    name: 'Bánh tráng rong biển',
+    originalPrice: 30000,
+    currentPrice: 25000,
+    discountPercentage: 15,
+    description: 'Bánh tráng rong biển được làm từ bánh tráng mỏng giòn, kết hợp với rong biển sấy khô, mè ngang và gia vị đậm đà. Mời bạn thưởng thức.',
+    variants: [{ weight: '200g' }],
+    images: [
+      'assets/images/products/banh-trang-rong-bien.png',
+      'assets/images/products/banh-trang-rong-bien.png',
+      'assets/images/products/banh-trang-rong-bien.png',
+      'assets/images/products/banh-trang-rong-bien.png'
+    ]
+  };
 
-describe('ProductDetailComponent', () => {
-  let component: ProductDetailComponent;
-  let fixture: ComponentFixture<ProductDetailComponent>;
+  relatedProducts = [
+    { 
+      name: 'Bánh tráng trộn mè chua', 
+      price: 35000, 
+      discount: 30,
+      image: 'assets/images/products/banh-trang-rong-bien.png'
+    },
+    { 
+      name: 'Bánh tráng rong rong mè nang', 
+      price: 25000, 
+      discount: 15,
+      image: 'assets/images/products/banh-trang-rong-bien.png'
+    },
+    { 
+      name: 'Bánh tráng trộn chả bông', 
+      price: 25000, 
+      discount: 15,
+      image: 'assets/images/products/banh-trang-rong-bien.png'
+    },
+    { 
+      name: 'Bánh tráng phô mai bơ', 
+      price: 25000, 
+      discount: 15,
+      image: 'assets/images/products/banh-trang-rong-bien.png'
+    }
+  ];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProductDetailComponent]
-    })
-    .compileComponents();
+  quantity = 1;
 
-    fixture = TestBed.createComponent(ProductDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  updateQuantity(change: number) {
+    this.quantity = Math.max(1, this.quantity + change);
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  addToCart() {
+    console.log('Added to cart');
+  }
+}
