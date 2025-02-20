63bb0748da9947ee44b4a81c7b7b5bb175e842fc
Bảo Trân <baotran@MacBook-Air-cua-BT.local>
2025-02-21 00:23:52 +0700

    Update minigame và product category

--- front-end/src/app/pages/product-category-backup1/product-category.component.ts
+++ front-end/src/app/pages/product-category-backup1/product-category.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-category',
-  standalone: true,
-  imports: [FormsModule, CommonModule],
   templateUrl: './product-category.component.html',
   styleUrls: ['./product-category.component.css']
 })
